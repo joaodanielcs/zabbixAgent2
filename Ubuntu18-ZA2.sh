e31223189abf9c19f8b3d4332432e26fb925b1cf
joaodanielcs <joaodanielcs@gmail.com>
2025-08-30 01:31:24 -0300

    Update Ubuntu18-ZA2.sh

--- Ubuntu18-ZA2.sh
+++ Ubuntu18-ZA2.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+IP="$1"
+[ -z "$IP" ] && read -p "Qual o IP do servidor Zabbix? " IP
+echo "🔄 Atualizando pacotes..."
+apt update -y > /dev/null 2>&1
+echo "📦 Instalando Zabbix Agent 2..."
 wget https://repo.zabbix.com/zabbix/7.4/release/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest_7.4+ubuntu18.04_all.deb
 dpkg -i zabbix-release_latest_7.4+ubuntu18.04_all.deb
 wget https://mirrors.edge.kernel.org/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
@@ -7,8 +14,10 @@ wget https://launchpad.net/ubuntu/+archive/primary/+files/libldap-2.4-2_2.4.45+d
 dpkg -i libldap-2.4-2_2.4.45+dfsg-1ubuntu1.11_amd64.deb
 apt --fix-broken install -y
 apt install zabbix-agent2 -y
-sed -i "s/^Server=.*/Server=192.168.0.105/" /etc/zabbix/zabbix_agentd.conf
-sed -i "s/^ServerActive=.*/ServerActive=192.168.0.105/" /etc/zabbix/zabbix_agentd.conf
+echo "🔧 Configurando o Zabbix Agent 2..."
+sed -i "s/^Server=.*/Server=$IP/" /etc/zabbix/zabbix_agentd.conf
+sed -i "s/^ServerActive=.*/ServerActive=$IP/" /etc/zabbix/zabbix_agentd.conf
 sed -i "s/^Hostname=.*/Hostname=$(hostname)/" /etc/zabbix/zabbix_agentd.conf
 systemctl enable zabbix-agent
 systemctl restart zabbix-agent
+echo "✅ Instalação concluída."
