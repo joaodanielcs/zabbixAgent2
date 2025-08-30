95860bd083b310c6f9ccc7d26db2dc16d8be9f11
joaodanielcs <joaodanielcs@gmail.com>
2025-08-30 01:18:02 -0300

    Update Debian-ZA2.sh

--- Debian-ZA2.sh
+++ Debian-ZA2.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 IP="$1"
-
-apt update
-apt install zabbix-agent2 -y
-
+[ -z "$IP" ] && read -p "Qual o IP do servidor Zabbix? " IP
+echo "🔄 Atualizando pacotes..."
+apt update -y > /dev/null 2>&1
+echo "📦 Instalando Zabbix Agent 2..."
+apt install zabbix-agent2 -y > /dev/null 2>&1
+echo "🔧 Configurando o Zabbix Agent 2..."
 sed -i "s/^Server=.*/Server=$IP/" /etc/zabbix/zabbix_agent2.conf
 sed -i "s/^ServerActive=.*/ServerActive=$IP/" /etc/zabbix/zabbix_agent2.conf
 sed -i "s/^Hostname=.*/Hostname=$(hostname)/" /etc/zabbix/zabbix_agent2.conf
 
-systemctl enable zabbix-agent2
-systemctl restart zabbix-agent2
+systemctl enable zabbix-agent2 > /dev/null 2>&1
+systemctl restart zabbix-agent2 > /dev/null 2>&1
+echo "✅ Instalação concluída."
